533fade64f8ff65b6be7f28f7f7fbdf24db97017
Dominique1211 <sunshine.p2525@gmail.com>
2022-09-13 01:06:42 +0000

    start

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  document.querySelector("#test").innerHTML = test();
+  function test() {
+    let pronouns = ["the", "our"];
+    let adjs = ["great", "big"];
+    let nouns = ["jogger", "racoon"];
+    let domains = [".com", ".net", ".gov", ".dog"];
+
+    for (let pronoun of pronouns) {
+      for (let adj of adjs) {
+        for (let noun of nouns) {
+          for (let domain of domains) {
+            console.log(pronoun + adj + noun + domain);
+          }
+        }
+      }
+    }
+  }
   console.log("Hello Rigo from the console!");
 };
